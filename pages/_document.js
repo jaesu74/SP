c034a2bee5e651e9c5ab9b1aa47a226da4106d72
jaesu74 <jaesu74@example.com>
2025-04-15 00:35:01 +0900

    Firebase 설정 복원 및 파비콘 추가

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <meta name="description" content="제재 정보 검색 시스템" />
+        <meta name="theme-color" content="#3B82F6" />
         <link rel="stylesheet" href="/css/custom.css" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
